ac21f75100fa340940ffb46b4fd28322472150bf
Saebyul1221 <dw696969@naver.com>
2021-11-08 16:38:04 +0900

    unused column

--- setup.sql
+++ setup.sql
@@ -1,13 +1,9 @@
 CREATE SCHEMA `otp` ;
 USE `otp`;
 
-/*
-password 컬럼은 Multi Account 대응용
-*/
 DROP TABLE IF EXISTS `userinfo`;
 CREATE TABLE `userinfo` (
   `user` VARCHAR(255) NOT NULL PRIMARY KEY,
-  `password` VARCHAR(255) NOT NULL,
   `program` VARCHAR(255) NOT NULL,
   `oauth` VARCHAR(255) NOT NULL,
   `used` BOOLEAN NOT NULL DEFAULT 0
